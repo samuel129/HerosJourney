1844a54c852c91ce377fadf78660759f3203c068
RileyDS <rileydonovanst@gmail.com>
2026-04-28 13:15:51 -0700

    Updated Collision + Mask Layers
    Should be more consistent now
    1. World
    2. Player
    3. Enemy (+ Projectiles)
    4. Player Attack Hitbox
    5. N/A
    6. Triggers (i.e. Stage Portal, Stage Triggers)

--- scripts/game.gd
+++ scripts/game.gd
@@ -468,7 +468,10 @@ func _spawn_stage_portal() -> void:
 	shape.radius = 22.0
 	collision.shape = shape
 	trigger.add_child(collision)
-	trigger.set_collision_mask_value(4, true)
+	trigger.set_collision_layer_value(6, true)
+	trigger.set_collision_mask_value(2, true)
+	trigger.monitoring = true
+	trigger.monitorable = true
 	trigger.body_entered.connect(_on_stage_portal_entered)
 	stage_portal_root.add_child(trigger)
 
